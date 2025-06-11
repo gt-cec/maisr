57cc34f7c63b757d616e95c04908f8ad86dfeaeb
ryanbowers166 <ryanbowers166@gmail.com>
2025-06-11 18:15:47 -0400

    Fixed 1000px/30tgt BC

--- run_training.py
+++ run_training.py
@@ -9,19 +9,19 @@ from utility.data_logging import load_env_config
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Run training with specified config file')
-    parser.add_argument('--config', '-c',
-                        type=str,
-                        default='',
-                        help='Path to the config file (default: config_files/june11a.json)')
-
-    args = parser.parse_args()
-    config_filename = args.config
+    # parser = argparse.ArgumentParser(description='Run training with specified config file')
+    # parser.add_argument('--config', '-c',
+    #                     type=str,
+    #                     default='',
+    #                     help='Path to the config file (default: config_files/june11a.json)')
+    #
+    # args = parser.parse_args()
+    # config_filename = args.config
 
     ############## ---- SETTINGS ---- ##############
     # Specify a checkpoint to load
     load_path = None  # './trained_models/6envs_obs-relative_act-continuous-normalized_lr-5e-05_bs-128_g-0.99_fs-1_ppoupdates-2048_curriculum-Truerew-wtn-0.02_rew-prox-0.005_rew-timepenalty--0.0_0516_1425/maisr_checkpoint_6envs_obs-relative_act-continuous-normalized_lr-5e-05_bs-128_g-0.99_fs-1_ppoupdates-2048_curriculum-Truerew-wtn-0.02_rew-prox-0.005_rew-timepenalty--0.0_0516_1425_156672_steps'
-    #config_filename = 'configs/june11a.json'
+    config_filename = 'configs/june11a.json'
     ###############################################
 
     # Get machine name to add to run name
@@ -37,17 +37,15 @@ if __name__ == "__main__":
 
 
     for levels_per_lesson in [{"0": 1, "1": 1, "2": 1}, {"0": 3, "1": 3, "2": 3}]:
-        for obs_type in ['absolute']:
-            config['obs_type'] = obs_type
-            config["n_eval_episodes"] = levels_per_lesson["0"]
+        config["n_eval_episodes"] = levels_per_lesson["0"]
 
-            print(f'\n--- Starting training run  ---')
+        print(f'\n--- Starting training run  ---')
 
-            train(
-                config,
-                n_envs=multiprocessing.cpu_count(),
-                load_path=load_path,
-                machine_name=machine_name,
-                project_name=project_name
-            )
-            print(f"✓ Completed training run")
+        train(
+            config,
+            n_envs=multiprocessing.cpu_count(),
+            load_path=load_path,
+            machine_name=machine_name,
+            project_name=project_name
+        )
+        print(f"✓ Completed training run")
