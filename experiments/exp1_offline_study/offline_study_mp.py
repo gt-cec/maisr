9928fcfe58e4f7c260e50d4bf10f753f148690d2
ryanbowers166 <ryanbowers166@gmail.com>
2026-02-27 11:25:55 -0500

    Added fcp

--- experiments/exp1_offline_study/offline_study_mp.py
+++ experiments/exp1_offline_study/offline_study_mp.py
@@ -581,8 +581,8 @@ def main():
     config['action_type'] = 'Discrete16'
 
     render = False
-    num_episodes = 30
-    num_human_episodes = 30
+    num_episodes = 10
+    num_human_episodes = 10
 
     # ── Pygame (main process only, for non-worker use) ─────────────────────
     pygame.font.init()
